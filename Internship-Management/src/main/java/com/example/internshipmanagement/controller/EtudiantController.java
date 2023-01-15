25c5d0d97d64fa1021d0ce355650fefc3bfa3b78
MouadAitBouzaid <aitbouzaid.mouad@gmail.com>
2023-01-16 00:06:43 +0100

    CRUD restController updated

--- Internship-Management/src/main/java/com/example/internshipmanagement/controller/EtudiantController.java
+++ Internship-Management/src/main/java/com/example/internshipmanagement/controller/EtudiantController.java
@@ -5,6 +5,7 @@ import com.example.internshipmanagement.entities.Etudiant;
 import com.example.internshipmanagement.mappers.EtudiantMapper;
 import com.example.internshipmanagement.repositories.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +18,47 @@ public class EtudiantController {
      private EtudiantMapper etudiantMapper;
      @Autowired
      private EtudiantRepository etudiantRepository;
-
+    /*
      @PostMapping("/etudiants/")
      public ResponseEntity<Etudiant> save(@RequestBody EtudiantDTO etudiantDTO){
          return new ResponseEntity<>(etudiantRepository.save(
                  etudiantMapper.fromDto(etudiantDTO)), HttpStatus.CREATED);
      }
+     */
+
+    @PostMapping("/etudiants/")
+    public Etudiant createEmployee(@RequestBody Etudiant etudiant) {
+        return etudiantRepository.save(etudiant);
+    }
+    /*
+    @GetMapping("/etudiants/")
+    public List<Etudiant> getAllEmployees() {
+        return etudiantRepository.findAll();
+    }
+
+     */
+
      @GetMapping("/etudiants/")
      public ResponseEntity<List<EtudiantDTO>> findAll(){
         return new ResponseEntity<>(etudiantMapper.listToDtos(etudiantRepository.findAll()),HttpStatus.OK);
      }
+
+    @PutMapping("/etudiants/{id}")
+    public EtudiantDTO updateEmployee(@PathVariable long id, @RequestBody EtudiantDTO etudiantDTO) {
+        Etudiant etudiant = etudiantRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id " + id));
+        etudiantMapper.updateEntityFromDto(etudiantDTO, etudiant);
+        return etudiantMapper.toDto(etudiantRepository.save(etudiant));
+    }
+     /*
+
+    @PutMapping("/etudiants/{id}")
+    public Etudiant updateEmployee(@PathVariable long id, @RequestBody Etudiant etudiant) {
+        etudiant.setId(id);
+        return etudiantRepository.save(etudiant);
+    }
+
+      */
 
 
     @GetMapping("/etudiants/{id}")
--- Internship-Management/src/main/java/com/example/internshipmanagement/mappers/EtudiantMapper.java
+++ Internship-Management/src/main/java/com/example/internshipmanagement/mappers/EtudiantMapper.java
@@ -3,6 +3,8 @@ package com.example.internshipmanagement.mappers;
 import com.example.internshipmanagement.dtos.EtudiantDTO;
 import com.example.internshipmanagement.entities.Etudiant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 
 import java.util.List;
@@ -12,4 +14,6 @@ public interface EtudiantMapper {
     EtudiantDTO toDto(Etudiant etudiant);
     List<EtudiantDTO> listToDtos (List<Etudiant> etudiant);
     Etudiant fromDto(EtudiantDTO etudiantDTO);
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(EtudiantDTO dto, @MappingTarget Etudiant etudiant);
 }
